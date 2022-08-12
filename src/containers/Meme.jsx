57fd081996d86fe06b7372e27b8aac8ecc0e31a5
Artem B <artem.bv@pm.me>
2022-08-12 20:30:00 +0300

    update meme container

--- src/containers/Meme.jsx
+++ src/containers/Meme.jsx
@@ -2,38 +2,49 @@ import React from "react"
 import memesData from "../memesData.js"
 
 export default function Meme() {
-    
-    const getRandomMeme = () => {
-        const rn = Math.floor(Math.random() * memesData.data.memes.length);
-        console.log(memesData.data.memes[rn].url);
-        return memesData.data.memes[rn].url;
-    }
-    const [state, setState] = React.useState(getRandomMeme());
 
-    const updateState = () => {
-        setState(getRandomMeme())
+    const getRandomMeme = () => {
+        const rn = Math.floor(Math.random() * allMemeImages.data.memes.length);
+        console.log(allMemeImages.data.memes[rn].url);
+        return allMemeImages.data.memes[rn].url;
+    }
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: getRandomMeme()
+    });
+
+    const updateMeme = () => {
+        setMeme({
+            ...meme,
+            randomImage: getRandomMeme()
+        })
     }
     return (
         <main>
             <div className="form">
                 <input 
+                    id="topText"
                     type="text"
                     placeholder="Top text"
                     className="form--input"
                 />
                 <input 
+                    id="bottomText"
                     type="text"
                     placeholder="Bottom text"
                     className="form--input"
                 />
                 <button 
                     className="form--button"
-                    onClick={updateState}
+                    onClick={updateMeme}
                 >
                     Get a new meme image 🖼
                 </button>
             </div>
-            <img src={state} className="meme--image"></img>
+            <img src={meme.randomImage} className="meme--image"></img>
         </main>
     )
 }
